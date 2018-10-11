40f60d070317a6220824f538ce571ca9d7cc36e9
Rafael Bayer <rafibayer7@gmail.com>
2018-10-10 21:56:52 -0700

    initial world generator and test viewer

--- Assets/WorldGenerator.cs
+++ Assets/WorldGenerator.cs
@@ -4,5 +4,105 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    
+    public int octaves = 5;
+    public float freq = 100.0f;
+    public float amp = 1;
+
+    public int width = 100;
+    public int height = 100;
+
+    public float[,] values;
+
+    public GameObject waterTile;
+    public GameObject groundTile;
+
+    private void Start()
+    {
+        values = new float[width, height];
+        generateNoiseMap();
+        noiseToTile();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach(Transform t in transform)
+            {
+                Destroy(t.gameObject);
+                generateNoiseMap();
+                noiseToTile();
+            }
+        }
+    }
+
+    public void generateNoiseMap()
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                float noise = 0.0f;
+                float gain = 1.0f;
+
+                for(int o = 0; o < octaves; o++)
+                {
+                    noise += Mathf.PerlinNoise(x * gain / freq, y * gain / freq) * amp / gain;
+                    gain *= 2.0f;
+
+                    values[x, y] = noise;
+
+                }
+
+            }
+        }
+
+       
+    }
+
+    public void noiseToTile()
+    {
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3 pos = new Vector3(x, 0, y);
+
+                if (values[x,y] < 0.9f)
+                {
+                    Instantiate(waterTile, pos, Quaternion.identity, transform);
+                }
+                else
+                {
+                    Instantiate(groundTile, pos, Quaternion.identity, transform);
+
+                }
+
+            }
+
+        }
+    }
+
+    public Texture2D getNoiseTexture()
+    {
+        generateNoiseMap();
+        Texture2D tex = new Texture2D(width, height);
+
+        Color[] pix = new Color[width * height];
+        int index = 0;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                pix[index] = new Color(values[x, y] * 255, values[x, y] * 255, values[x, y] * 255);
+                index++;
+            }
+        }
+
+        tex.SetPixels(pix);
+        tex.Apply();
+        return tex;
+    }
 }
