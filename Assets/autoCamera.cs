d89d2021785fd8d844f966e283224bfc17735795
Rafael Bayer <rafibayer7@gmail.com>
2018-10-11 15:56:09 -0700

    fixed read to not rotate world, added autocamera to view camera to world

---
+++ Assets/autoCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoCamera : MonoBehaviour {
+
+    private WorldGenerator worldGen;
+	void Start () {
+        StartCoroutine(delayedCamera());
+    }
+
+    IEnumerator delayedCamera()//waits until worldGen finished generation or read
+    {
+        yield return new WaitForSeconds(0.1f);
+        worldGen = GameObject.Find("worldGen").GetComponent<WorldGenerator>();
+        Vector3 pos = new Vector3(worldGen.width / 2,
+                                 Mathf.Sqrt(worldGen.width * worldGen.height),
+                                    worldGen.height / 2);
+        transform.position = pos;
+        yield break;
+    }
+	
+}
