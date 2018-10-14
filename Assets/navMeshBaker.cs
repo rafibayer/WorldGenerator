d9fbc674b0bcfe922ab0874bbcee53ad503cd4a4
Rafael Bayer <rafibayer7@gmail.com>
2018-10-14 12:13:19 -0700

    added navmesh agent assets and navmeshbaker for runtime navmesh generation

---
+++ Assets/navMeshBaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navMeshBaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LateStart());
+	}
+	
+	IEnumerator LateStart()
+    {
+        yield return new WaitForSeconds(0.1f);
+        foreach(Transform surface in transform)
+        {
+            surface.GetComponent<NavMeshSurface>().BuildNavMesh();
+        }
+    }
+}
