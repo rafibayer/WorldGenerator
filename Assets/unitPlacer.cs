b3d1e07f7c8a0d709c5716b2adeb3ed991276e2f
Rafael Bayer <rafibayer7@gmail.com>
2018-10-15 13:41:23 -0700

    added checkTile and checkTiles to unitPlacer, also added an overload for placeUnit that places units that take up multiple tiles

--- Assets/unitPlacer.cs
+++ Assets/unitPlacer.cs
@@ -45,6 +45,60 @@ public class unitPlacer : MonoBehaviour {
 
     }
 
+    //places a unit at xPlace, yPlace, occupying from xStart, yStart to xTo, yTo (inclusive)
+    //returns true if placed succesfully, returns false otherwise
+    public bool placeUnit(GameObject unit, int xPlace, int yPlace, 
+                            int xStart, int yStart, int xTo, int yTo)
+    {
+        if(xPlace < xStart || yPlace < yStart || xPlace > xTo || yPlace > yTo)
+        {
+            throw new System.ArgumentException(
+                "xPlace and yPlace must be within xStart, yStart and xTo, yTo");
+        }
+
+        if(!checkTiles(xStart, yStart, xTo, yTo))
+        {
+            return false;
+        }
+
+        for(int x = xStart; x <= xTo; x++)
+        {
+            for(int y = yStart; y <= yTo; y++)
+            {
+                occupied[x, y] = true;
+            }
+        }
+
+        Instantiate(unit, new Vector3(xPlace, 1, yPlace), Quaternion.identity);
+        return true;
+
+    }
+
+    //returns true if tile at x, y is open, returns false otherwise
+    public bool checkTile(int x, int y)
+    {
+        return occupied[x, y];
+    }
+
+    //returns true if tiles between x,y and xTo, yTo are open (inclusive)
+    //returns false otherwise
+    public bool checkTiles(int xStart, int yStart, int xTo, int yTo)
+    {
+        for(int x = xStart; x <= xTo; x++)
+        {
+            for(int y = yStart; y <= yTo; y++)
+            {
+                if(occupied[x, y])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    //start late so that worldGen can generate the world first
     IEnumerator LateStart()
     {
         yield return new WaitForSeconds(0.1f);
